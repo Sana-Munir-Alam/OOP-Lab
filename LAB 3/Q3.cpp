93949c37bb81748eef939f2ef80de3edb3f1ecd0
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-02-07 09:50:24 +0500

    Create Q3.cpp

---
+++ LAB 3/Q3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Glass{
+	public:
+		int LiquidLevel;
+		
+		void SetLiquid(int Liquid){
+			LiquidLevel = Liquid;
+		}
+		void RefillDrink(){
+			LiquidLevel = 200;
+			cout << "Your Glass Has been Refilled to 200ml...Enjoy!" << endl;
+		}
+		
+		void DrinkLevel(int millilitres){
+			if ((LiquidLevel - millilitres) < 100){
+				cout << "Your Glass level has dropped below 100ml. Allow me to Refill!" <<endl;
+				RefillDrink();
+			}else{
+				cout << "No need to refill, you still have " << LiquidLevel - millilitres << "ml left...Enjoy!"<< endl;
+			}
+		}
+};
+
+int main(int argc, char* argv[]){
+	if (argc != 2){
+		cout << "Incorrect data Entered!!" << endl << "Kindly Enter the amount of Drink intake";
+		return 1;
+	}
+	Glass Info;
+	Info.SetLiquid(200);
+	Info.DrinkLevel(stoi(argv[1]));
+	return 0;
+}
