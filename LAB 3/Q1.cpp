a116f16d7b0ddff2ed5c497bbda86590ae1d3fb3
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-02-07 09:21:44 +0500

    Add files via upload

---
+++ LAB 3/Q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class User{
+	public:
+		int Age;
+		string Name;
+};
+
+int main(int argc, char* argv[]){
+	if(argc != 3){
+		cout << "Incorrect Data Entered!! Enter First Name and Age Only Respectiveley!!";
+		return 1;
+	}
+	User Info;
+	Info.Name = argv[1];
+	Info.Age = stoi(argv[2]);
+	cout << "My name is " << Info.Name << " and I'm " << Info.Age << " years old.";
+	return 0;
+}
