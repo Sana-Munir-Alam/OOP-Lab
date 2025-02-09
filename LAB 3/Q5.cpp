a7fba0618dab03665287bfaffb4e037ed6e77524
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-02-09 05:12:53 -0800

    Update Q5.cpp

--- LAB 3/Q5.cpp
+++ LAB 3/Q5.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Book{
@@ -8,57 +9,64 @@ class Book{
 		string Publisher;
 		int ISBN;
 	public:
-		void setBookName(string BookName){
+		void SetBookName(string BookName){
 			this->BookName = BookName;
 		}
 		string GetBookName(){
 			return BookName;
 		}
-		void setAuthorName(string AuthorName){
+		void SetAuthorName(string AuthorName){
 			this->AuthorName = AuthorName;
 		}
 		string GetAuthorName(){
 			return AuthorName;
 		}
-		void setPublisher(string Publisher){
+		void SetPublisher(string Publisher){
 			this->Publisher = Publisher;
 		}
 		string GetPublisher(){
-			return Pub;
+			return Publisher;
 		}
-		void setISBN (int ISBN){
+		void SetISBN (int ISBN){
 			this->ISBN = ISBN;
 		}
-		int ISBN (){
+		int GetISBN (){
 			return ISBN;
 		}
-		void GetBookInfo(){
-			
+		string GetBookInfo(){
+			return "Book Name: " + BookName + "\n" +
+               "Author Name: " + AuthorName + "\n" +
+               "Publisher: " + Publisher + "\n" +
+               "ISBN: " + to_string(ISBN) + "\n";
 		}
 };
 
 int main(int argc, char* argv[]){
-	if (argc != 7){
-		cout << "Incorrect Data Format Entered!! Kinldy follow this pattern" << endl;
-		return 1;
-	}
-	
-	Book BookTest[5];
+    Book BookTest[5];
 	for (int i = 0; i < 5; i++){
 		string B = "",A = "",P = "";
 		int Code;
+        cout << "Enter details for Book " << i + 1 << endl;
 		cout << "Enter Book Name: ";
 		getline(cin, B);
-		BookTest[i].setBookName(B);
+		BookTest[i].SetBookName(B);
 		cout << "Enter Author Name: ";
 		getline(cin, A);
-		BookTest[i].setAuthorName(A);
+		BookTest[i].SetAuthorName(A);
 		cout << "Enter Publisher Name: ";
 		getline(cin, P);
-		BookTest[i].setPublisherName(P);
-		cout << "Enter Author Name: ";
-		cin << Code;
-		BookTest[i].setISBN(Code)
+		BookTest[i].SetPublisher(P);
+		cout << "Enter ISBN: ";
+		cin >> Code;
+        cin.ignore();
+		BookTest[i].SetISBN(Code);
+        cout << endl;
 	}
+    cout << "-----------------------------------"<<endl;
+    cout << "Book Information:\n";
+    for (int i = 0; i < 5; i++) {
+        cout << "Book " << i + 1 << ":\n";
+        cout << BookTest[i].GetBookInfo() << endl;
+    }
 	return 0;
 }
