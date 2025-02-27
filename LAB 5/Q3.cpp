4fa2853dea386180775d29215edf512c8a283657
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-02-27 19:46:05 +0500

    Update Q3.cpp

--- LAB 5/Q3.cpp
+++ LAB 5/Q3.cpp
@@ -1,114 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
-class Book {
-public:
-    string title;
-    string author;
-    string isbn;
-
-    Book(string t, string a, string i) : title(t), author(a), isbn(i) {}
-
-    void displayBookInfo() {
-        cout << "Title: " << title << ", Author: " << author << ", ISBN: " << isbn << endl;
-    }
+class Car{
+    private:
+        string Name;
+		int ID;
+	public:
+		Car(){
+			// Default Constructor
+			cout << "Car Constructor Called!" << endl;
+		}
+		Car(int CarID, string CarName) : Name(CarName), ID(CarID){
+			// Parmatrized Constructor
+		}
+		~Car() {
+	        cout << "Car destructed!" << endl;
+	    }
+		void Display() const {
+			cout << "Car ID: " << ID << endl << "Car Name: " << Name << endl;
+		}
+        string GetName() const { return Name; }
+        int GetID() const { return ID; }
 };
 
-class Catalog {
-public:
-    vector<Book*> books;
-
-    void addBook(Book* book) {
-        books.push_back(book);
-    }
-
-    Book* findBookByISBN(const string& isbn) {
-        for (auto& book : books) {
-            if (book->isbn == isbn) {
-                return book;
-            }
-        }
-        return nullptr;
-    }
+class Garage{
+	private:
+		string GarageName;
+		vector<Car*> Info;
+	public:
+		Garage(){
+			// Default Constructor
+			cout << "Garage Constructor Called!" << endl;
+		}
+		Garage(string Name) : GarageName(Name){
+			// Paramatrzed Constructor
+			cout << "Garage Constructor Called!" << endl;
+		}
+		~Garage() {
+	        cout << "Garage Destructed!" << endl;
+	    }
+		void ParkCar(Car * C){
+			Info.push_back(C);
+		}
+		void ListCars() const {
+			cout << "Garage Name: " << GarageName << endl;
+			for (auto car : Info){
+				car -> Display();
+			}
+		}
 };
 
-class Library {
-private:
-    Catalog catalog;
-
-public:
-    string name;
-    string address;
-    vector<Book*> bookCollection;
-
-    Library(string n, string a) : name(n), address(a) {}
-
-    void addBook(Book* book) {
-        bookCollection.push_back(book);
-        catalog.addBook(book); 
-    }
-
-    void removeBook(Book* book) {
-        for (auto it = bookCollection.begin(); it != bookCollection.end(); ++it) {
-            if (*it == book) {
-                bookCollection.erase(it);
-                break;
-            }
-        }
-
-        for (auto it = catalog.books.begin(); it != catalog.books.end(); ++it) {
-            if (*it == book) {
-                catalog.books.erase(it);
-                break;
-            }
-        }
-    }
-
-    Book* findBookInCatalog(const string& isbn) {
-        return catalog.findBookByISBN(isbn);
-    }
-
-    void showLibraryInfo() {
-        cout << "Library Name: " << name << endl;
-        cout << "Library Address: " << address << endl;
-    }
-
-    void displayAllBooks() {
-        cout << "Books in Library:" << endl;
-        for (auto& book : bookCollection) {
-            book->displayBookInfo();
-        }
-    }
-};
-
-int main() {
-    Book book1("Sense and Sensibility", "Jane Austen", "123456");
-    Book book2("Jane Eyre", "Charlotte Bronte", "654321");
-    Book book3("Hobbit", "Tolkeins", "233445");
-
-    Library library("Central Library", "123 Library St.");
-
-    library.addBook(&book1);
-    library.addBook(&book2);
-    library.addBook(&book3);
-
-    library.showLibraryInfo();
-    library.displayAllBooks();
-
-    string searchIsbn = "654321";
-    Book* foundBook = library.findBookInCatalog(searchIsbn);
-    if (foundBook) {
-        cout << "Found Book: ";
-        foundBook->displayBookInfo();
-    } else {
-        cout << "Book with ISBN " << searchIsbn << " not found in catalog." << endl;
-    }
-
-    library.removeBook(&book2);
-    cout << "\nAfter removing a book:\n";
-    library.displayAllBooks();
-
-    return 0;
+int main(){
+	Car C1(123, "Toyota");
+	Car C2(321, "City");
+	Car C3(394, "Vigo");
+	Garage Info("Sunshine Garage");
+	Info.ParkCar(&C1);
+	Info.ParkCar(&C2);
+	Info.ParkCar(&C3);
+	Info.ListCars();
 }
