d025cc0500da0c67d0ec3a3c9347a5062380ee32
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2025-05-02 09:31:14 +0500

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <any>
+#include <exception>
+using namespace std;
+
+// Custom Exception for Bad Type
+class BadTypeException : public exception {
+public:
+    const char* what() const noexcept override {
+        return "Bad Type Exception!!";
+    }
+};
+
+// Type-Safe Container class
+class TypeSafeContainer {
+    private:
+        std::any AnyTypeVariable;
+    public:
+        template <typename T>
+        void store(const T& value) {
+            AnyTypeVariable = value;
+        }
+        
+        template <typename T>
+        T get() const {
+            try {
+                return std::any_cast<T>(AnyTypeVariable);
+            }
+            catch (const std::bad_any_cast&) {
+                throw BadTypeException();
+            }
+        }
+};
+
+// Test the container
+int main() {
+    try {
+        TypeSafeContainer container;
+        
+        container.store<int>(42);
+        int myInt = container.get<int>();
+        cout << "Stored int: " << myInt << endl;
+
+        // double myDouble = container.get<double>();  // This will throw a BadTypeException
+        // cout << "Stored double: " << myDouble << endl;
+
+    }
+    catch (const exception& e) {
+        cout << "Error: " << e.what() << endl;
+    }
+    return 0;
+}
